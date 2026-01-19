44953e5e8fa4924bac84ef1b48d3bf7567facd0d
phatlefreec <39773716+phattanle2009@users.noreply.github.com>
2026-01-19 10:08:17 +0700

    chore: init pod

---
+++ the-mobile-workshop/Common/Extension/UITableView.swift
@@ -0,0 +1,26 @@
+//
+//  UITableView.swift
+//  the-mobile-workshop
+//
+//  Created by Le Tan Phat on 19/1/26.
+//
+
+import UIKit
+
+extension UITableView {
+    convenience init(
+        tableStyle: UITableView.Style = .plain,
+        backgroundColor: UIColor = .white,
+        separator: UITableViewCell.SeparatorStyle = .none
+    ) {
+        self.init(frame: .zero, style: tableStyle)
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.backgroundColor = backgroundColor
+        self.separatorStyle = separator
+        self.estimatedRowHeight = 44
+        self.rowHeight = UITableView.automaticDimension
+        self.keyboardDismissMode = .onDrag
+        self.showsVerticalScrollIndicator = false
+        self.showsHorizontalScrollIndicator = false
+    }
+}
